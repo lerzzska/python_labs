6f4f76fa6350914178d1de2f614b3189de79d8a8
lerzzska <lyubovrekutina@yandex.ru>
2026-09-17 20:21:03 +0300

    lab

---
+++ scr/lab01/ex02.py
@@ -0,0 +1,7 @@
+chislo1=(input('a=')).replace(',', '.')
+ch1=float(chislo1)
+chislo2=(input('b=')).replace(',', '.')
+ch2=float(chislo2)
+sum=ch1+ch2
+avg=sum/2
+print(f'sum={sum:.2f}; avg={avg:.2f}')
